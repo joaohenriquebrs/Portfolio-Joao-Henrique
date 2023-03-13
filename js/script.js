bb7baaf2e0b781b5d2d4e0effbce67ec5c023ba2
João Henrique Portela <jhportla_rio32@hotmail.com>
2023-03-13 20:11:54 -0300

    Acrescentei mais 6 seções e algumas funções utilizando JavaScript e jQuery

--- js/script.js
+++ js/script.js
@@ -12,7 +12,22 @@ function scrollToIdOnClick(event){
     const to = document.querySelector(id).offsetTop;
     
     window.scroll({
-        top: to - 45,
+        top: to - 75,
         behavior: "smooth",
     });
 }
+
+const botaoCopiar = document.querySelectorAll(".botaoCopiar");
+botaoCopiar.forEach(item => {
+    item.addEventListener('click', ()=> {
+        if(navigator.clipboard.writeText(item.value)) {
+            item.id = "email-copiado_check"
+            item.textContent = "Email copiado";
+        }
+    
+        setInterval(()=> {
+            item.id= "email-copiado_check";
+            item.textContent = "Copiar email"
+        }, 4000);
+    })
+})
